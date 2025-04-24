4a575ff507adf4d673ade6d7a64f7fbf406cef30
Aman singh tomar <129831633+aman812642@users.noreply.github.com>
2025-04-24 11:13:36 +0530

    Update user.js

--- model/user.js
+++ model/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MONGODB_URI = `mongodb://127.0.0.1:27017/miniproject`
+const MONGODB_URI = "mongodb://127.0.0.1:27017/miniproject"
 mongoose.connect(process.env.MONGODB_URI);
 
 const userSchema = mongoose.Schema({
